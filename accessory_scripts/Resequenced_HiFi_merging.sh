0860b3784fc7028cd364ad2ab215c19d0d92b374
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-09-10 10:38:33 -0700

    Create Resequenced_HiFi_merging.sh

---
+++ accessory_scripts/Resequenced_HiFi_merging.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=grouse_hifi
+#SBATCH -A fnrdewoody
+#SBATCH -t 15:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=40G
+#SBATCH -p cpu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=blackan@purdue.edu
+
+ml bioconda
+ml anaconda
+conda activate pbtk
+
+#pbmerge -o F578_MERGED.bam F5478_m84221_260820_001659_s4.hifi_reads.bc2174.bam F5478_m84221_260901_214100_s2.hifi_reads.bc2174.bam
+
+#pbmerge -o F5480_MERGED.bam F5480_pool_10_m84221_260821_081306_s3.hifi_reads.bc2175.bam F5480_pool_9_m84221_260821_060307_s2.hifi_reads.bc2175.bam
+
+#pbmerge -o F5502_MERGED.bam F5502_m84221_260821_081306_s3.hifi_reads.bc2177.bam F5502_m84221_260901_214100_s2.hifi_reads.bc2177.bam
+
+pbmerge -o F5462_MERGED.bam F5462_m84221_260811_163620_s4.hifi_reads.bc2170.bam F5462_m84221_260901_214100_s2.hifi_reads.bc2170.bam 
